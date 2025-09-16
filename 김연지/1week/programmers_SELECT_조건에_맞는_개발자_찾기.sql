7ed321d32bc03fb00b06e1870b6b8a383d6484e7
[Cloud3] 김연지 <yjuio8357@naver.com>
2025-09-16 22:27:50 +0900

    chore: {김연지} programmers [SELECT] 조건에 맞는 개발자 찾기

--- 김연지/1week/programmers_SELECT_조건에_맞는_개발자_찾기.sql
+++ 김연지/1week/programmers_SELECT_조건에_맞는_개발자_찾기.sql
@@ -1,6 +1,7 @@
 1. BIT 연산으로 풀기
 2. SKILL_CODE에 python의 포함여부를 알려면 (개발자의 스킬코드 & Python 코드)를 해서 Python 스킬코드가 연산 결과로 나오면 해당 스킬을 보유했다는 뜻.
-3. WHERE 조건절에 스칼라 서브쿼리를 넣어 사용
+3. 파이썬 -> 10진수로 4라면 2진수로
+4. WHERE 조건절에 스칼라 서브쿼리를 넣어 사용
 
 
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
